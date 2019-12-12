88bfdb1c5fca4d1dc453fc0439d3ed9a992d4c65
David Meir-Levy <davidmeirlevy@gmail.com>
2019-12-12 15:59:08 +0200

    init-data for staging envs

--- helpers/init-data.js
+++ helpers/init-data.js
@@ -1,24 +1,37 @@
 const { spawn, fork } = require('child_process')
-const {mkdirSync} = require('fs');
+const { mkdirSync } = require('fs')
 const apps = require('./apps')
-console.log('run mongo')
-mkdirSync('db-data');
-const mongo = spawn('mongod', ['--dbpath', './db-data'])
 
-process.once('exit', () => mongo.kill())
+const isDev = process.env.NODE_ENV === 'development'
 
-mongo.stdout.on('data', data => {
-  if (data.toString().includes('waiting for connections on port')) {
-    console.log('db is running, start populate data')
-    Promise.all([
-      runServiceInit('content', apps.content.env),
-      runServiceInit('auth', apps.auth.env)
-    ]).then(() => {
-      console.log('init data completed successfully!')
-      process.exit(0)
-    })
-  }
-})
+if (isDev) {
+  // run mongo locally to insert data.
+  console.log('run mongo')
+  mkdirSync('db-data')
+  const mongo = spawn('mongod', ['--dbpath', './db-data'])
+
+  process.once('exit', () => mongo.kill())
+
+  mongo.stdout.on('data', data => {
+    if (data.toString().includes('waiting for connections on port')) {
+      console.log('db is running')
+      runServicesInits()
+    }
+  })
+} else {
+  runServicesInits()
+}
+
+function runServicesInits () {
+  console.log('start populate data')
+  Promise.all([
+    runServiceInit('content', apps.content.env),
+    runServiceInit('auth', apps.auth.env)
+  ]).then(() => {
+    console.log('init data completed successfully!')
+    process.exit(0)
+  })
+}
 
 function runServiceInit (service, env) {
   console.log('run', service)
