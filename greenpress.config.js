0939d7397a454f2eb95373fc979317128c1812a5
David Meir-Levy <davidmeirlevy@gmail.com>
2020-08-25 11:36:49 +0300

    upgraded cli-services

--- greenpress.config.js
+++ greenpress.config.js
@@ -1,13 +1,25 @@
 const IS_PROD = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  frontTheme: process.env.FRONT_THEME || 'classic', // manual change of theme
-  scripts: {
-    secrets: process.env.SECRETS_SERVICE_SCRIPT || './secrets/index.js',
-    auth: process.env.AUTH_SERVICE_SCRIPT || './auth/index.js',
-    content: process.env.CONTENT_SERVICE_SCRIPT || './content/index.js',
-    assets: process.env.ASSETS_SERVICE_SCRIPT || './assets/index.js',
-    front: process.env.FRONT_SERVICE_SCRIPT || (IS_PROD ? 'cd blog-front && npm start' : 'cd blog-front && npm run dev'),
-    admin: process.env.ADMIN_SERVICE_SCRIPT || IS_PROD ? './admin/server.js' : 'cd admin && npm run serve',
+  services: {
+    front: {
+      theme: process.env.FRONT_THEME || 'classic', // manual change of theme
+      cwd: process.env.FRONT_SERVICE_CWD || './blog-front'
+    },
+    secrets: {
+      cwd: process.env.SECRETS_SERVICE_CWD || './secrets'
+    },
+    auth: {
+      cwd: process.env.AUTH_SERVICE_CWD || './auth'
+    },
+    content: {
+      cwd: process.env.CONTENT_SERVICE_CWD || './content'
+    },
+    assets: {
+      cwd: process.env.ASSETS_SERVICE_CWD || './assets'
+    },
+    admin: {
+      cwd: process.env.ADMIN_SERVICE_CWD || './admin'
+    }
   }
 }
